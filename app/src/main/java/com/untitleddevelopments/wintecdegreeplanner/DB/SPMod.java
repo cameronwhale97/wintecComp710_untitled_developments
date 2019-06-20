a55f69ed6e5908027abdafb35b4a85cad5a5efcc
= <geoffgenner@outlook.com>
2019-06-21 10:42:54 +1200

    Working on padlocks showing properly

--- app/src/main/java/com/untitleddevelopments/wintecdegreeplanner/DB/SPMod.java
+++ app/src/main/java/com/untitleddevelopments/wintecdegreeplanner/DB/SPMod.java
@@ -1,10 +1,8 @@
 package com.untitleddevelopments.wintecdegreeplanner.DB;
 
-import android.util.Log;
+import com.untitleddevelopments.wintecdegreeplanner.global.Globals;
 
 import java.util.ArrayList;
-
-import static android.support.constraint.Constraints.TAG;
 
 public class SPMod {
     private int module_ID;
@@ -12,16 +10,17 @@ public class SPMod {
     private String name;
     private ArrayList<PreReq> preReqs;
     private Boolean completed;
-
+    private boolean locked;
 
     //constructor full
 
-    public SPMod(int module_ID, String code, String name, ArrayList<PreReq> preReqs, Boolean completed) {
+    public SPMod(int module_ID, String code, String name, ArrayList<PreReq> preReqs, Boolean completed, Boolean locked) {
         this.module_ID = module_ID;
         this.code = code;
         this.name = name;
         this.preReqs = preReqs;
         this.completed = completed;
+        this.locked = locked;
     }
     //blank constructor
     public SPMod(){}
@@ -66,6 +65,14 @@ public class SPMod {
         this.completed = completed;
     }
 
+    public Boolean getLocked() {
+        return locked;
+    }
+
+    public void setLocked(Boolean locked) {
+        this.locked = locked;
+    }
+
     public int getYear() {
         return (Integer.parseInt(this.code.substring(4,5) ) -4 );
 
@@ -88,6 +95,19 @@ public class SPMod {
             }
         }
         return preString;
-    }
+    } //toStringPreReqs
+
+    public static Boolean preReqsAreDone(ArrayList<PreReq> preReqs){
+        Boolean done = true;
+        if(preReqs == null){
+            done = true; //there are no prereqs we say they are done
+        } else {
+            for (PreReq pReqSingle : preReqs){
+                if(!Module.isCompleted(Globals.getStudent_ID(), pReqSingle.getPreReq_ID())) done = false;
+                //so if any pre-req is not completed our preReqs are not done
+            }
+        }
+        return done;
+    } //preReqsAreDone
 }
 
