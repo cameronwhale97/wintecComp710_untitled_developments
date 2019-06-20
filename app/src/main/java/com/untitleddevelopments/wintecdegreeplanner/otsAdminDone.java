6578a03f0301838192207f4c6b3a632d4b72669a
Cameron Whale <cwhale@me.com>
2019-06-20 23:57:03 +1200

    ots push somewhat working? still WIP student view xml has appeared.

--- app/src/main/java/com/untitleddevelopments/wintecdegreeplanner/otsAdminDone.java
+++ app/src/main/java/com/untitleddevelopments/wintecdegreeplanner/otsAdminDone.java
@@ -1,33 +1,28 @@
 package com.untitleddevelopments.wintecdegreeplanner;
 
-import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
-import com.untitleddevelopments.wintecdegreeplanner.DB.DBHelper;
-import com.untitleddevelopments.wintecdegreeplanner.DB.DBManager;
-import com.untitleddevelopments.wintecdegreeplanner.global.PrefsManager;
 import com.untitleddevelopments.wintecdegreeplanner.ui.StuPlan.StuPlanActivity;
 
-public class otsStudentDone extends AppCompatActivity {
+public class otsAdminDone extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ots_student_done);
+        setContentView(R.layout.activity_ots_admin_done);
 
         ImageView otsDone = findViewById(R.id.imageView6);
 
-        otsDone.setOnClickListener(new View.OnClickListener() {
+        otsDone.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
-                Intent StuPlanActivity = new Intent(otsStudentDone.this, StuPlanActivity.class);
+                Intent StuPlanActivity = new Intent(otsAdminDone.this, StuPlanActivity.class);
                 startActivity(StuPlanActivity);
             }
         });
