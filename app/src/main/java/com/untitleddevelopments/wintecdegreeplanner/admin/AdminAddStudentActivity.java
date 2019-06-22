d7a1c7d4f2a51b725d0a2d67d50278ec88eb2721
Navi Singh <navisinghnz9@gmail.com>
2019-06-22 15:38:49 +1200

    added stream dropdown & start date to add student

--- app/src/main/java/com/untitleddevelopments/wintecdegreeplanner/admin/AdminAddStudentActivity.java
+++ app/src/main/java/com/untitleddevelopments/wintecdegreeplanner/admin/AdminAddStudentActivity.java
@@ -6,7 +6,9 @@ import android.support.v7.widget.PopupMenu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.untitleddevelopments.wintecdegreeplanner.R;
@@ -37,6 +39,31 @@ public class AdminAddStudentActivity extends AppCompatActivity implements View.O
 
         btnAdd.setOnClickListener(this);
         btnMenu.setOnClickListener(this);
+
+        populateStreamsInDropdownList();
+    }
+
+
+    private void populateStreamsInDropdownList() {
+
+        // Reference: stackoverflow.com
+        // https://stackoverflow.com/questions/13377361/how-to-create-a-drop-down-list
+
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.spStreams);
+
+        //create a list of items for the spinner.
+        String[] items = new String[] {"1", "2", "three"};
+
+        // Create an adapter to describe how the items are displayed,
+        // adapters are used in several places in android.
+        // There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item, items);
+
+        // Set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+
     }
 
     private void saveStudentInDB() {
